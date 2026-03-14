89b88f3fd8aa44c646bf1040d980f446bdd20d81
Бєлоглазов Олександр <beloalek@gmail.com>
2026-03-14 20:02:22 +0200

    Added new functionality

--- Repositories/post.py
+++ Repositories/post.py
@@ -4,20 +4,21 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from Database.models.post import Post
+from Database.models.summary_post import SummaryPost
 
 class PostRepository:
     def __init__(self, session: AsyncSession):
         self.__session = session
 
-    async def get_content_from_channel_id(self, channel_id: int):
-        statement = select(Post.content).where(channel_id == Post.channel_id).order_by(Post.created_at.desc())
-        return await self.__session.scalars(statement)
+    async def get_post_content(self, post_id: int) -> str:
+        statement = select(Post.content).where(post_id == Post.tg_id)
+        return await self.__session.scalar(statement)
 
     async def get_post_by_tg_id(self, tg_id: int):
         statement = select(Post).where(tg_id == Post.tg_id)
         return await self.__session.scalar(statement)
 
-    async def get_datetime_by_tg_id(self, tg_id: int) -> datetime:
+    async def get_datetime(self, tg_id: int) -> datetime:
         statement = select(Post.created_at).where(Post.tg_id == tg_id)
         return await self.__session.scalar(statement)
 
@@ -37,3 +38,43 @@ class PostRepository:
                     content=content,
                     created_at=created_at)
         self.__session.add(post)
+
+    async def get_posts_list(self, channel_id: int, limit: int) -> list[Post]:
+        statement = select(Post).where(channel_id == Post.channel_id).limit(limit).order_by(Post.created_at.desc())
+        result = await self.__session.execute(statement)
+        return result.scalars().all()
+
+    async def filter_unprocessed_posts(self, channel_id: int, new_posts: list[Post]) -> list[Post]:
+        """
+        Приймає список постів з Telegram.
+        Повертає список лише тих ПОСТІВ, які ЩЕ НЕ прив'язані до жодного дайджесту.
+        """
+        # Якщо прийшов порожній список, одразу повертаємо порожній
+        if not new_posts:
+            return []
+
+        new_post_ids = [post.tg_id for post in new_posts]
+
+        # Шукаємо "старі" пости, які вже є в базі
+        statement = (
+            select(Post.tg_id)
+            .join(SummaryPost, Post.tg_id == SummaryPost.post_id)
+            .where(
+                Post.channel_id == channel_id,
+                # Шукаємо тільки серед тих ID, які щойно зібрав скрапер
+                Post.tg_id.in_(new_post_ids)
+            )
+        )
+
+        result = await self.__session.execute(statement)
+
+        # existing_ids міститиме список tg_id, які ВЖЕ оброблені
+        existing_ids = result.scalars().all()
+
+        # Відсіюємо оброблені пости від нових
+        unprocessed_posts = [
+            post for post in new_posts
+            if post.tg_id not in existing_ids
+        ]
+
+        return unprocessed_posts
