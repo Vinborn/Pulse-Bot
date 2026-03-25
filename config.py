0ab0dd144f1b82d74931d6c06022f03fe5d2ce9b
Бєлоглазов Олександр <beloalek@gmail.com>
2026-03-25 18:41:19 +0200

    Added phone_number and session_name

--- config.py
+++ config.py
@@ -6,9 +6,11 @@ load_dotenv()
 
 @dataclass
 class Config:
+    phone_number: str = os.getenv("PHONE_NUMBER")
     bot_token: str = os.getenv("BOT_TOKEN")
     api_id: int = int(os.getenv("API_ID"))
     api_hash: str = os.getenv("API_HASH")
+    session_name: str = os.getenv("SESSION_NAME")
     db_url: str = os.getenv("DB_URL")
     ai_api_key: str = os.getenv("AI_API_KEY")
 
