150f650a27f18623dcb26661e08985fcddd813cc
Бєлоглазов Олександр <beloalek@gmail.com>
2026-03-25 18:43:45 +0200

    Reworked the channel availability check.

--- Handlers/add.py
+++ Handlers/add.py
@@ -18,28 +18,36 @@ CHANNEL_RE = r"(?:https?://t\.me/|@)([a-zA-Z0-9_]{5,})"
 async def add(message: types.Message, state: FSMContext):
     await state.set_state(AddChannel.waiting_for_link)
     await message.answer(
-        "Готовий розширити твій інформаційний потік!\n"
-        "Надішліть мені посилання на Telegram-канал (наприклад, @channel_name або t.me/link)."
+        "↔️ Готовий розширити твій інформаційний потік!\n"
+        "Надішли мені посилання на Telegram-канал (наприклад, @channel_name або t.me/link)."
     )
 
 # сработает ТОЛЬКО в состоянии ожидания ссылки
 @router.message(AddChannel.waiting_for_link)
 async def handle_channel_link(message: types.Message, state: FSMContext, channel_repo: ChannelRepository, subscription_repo: UserSubscriptionRepository):
     """ЛОГИКА ДОБАВЛЕНИЯ КАНАЛА В БАЗУ"""
+    # Знаходимо посилання на канал
     channel_match = re.search(CHANNEL_RE, message.text)
 
     if channel_match:
         link = channel_match.group(0)
         await message.answer(f"Почав обробку каналу...")
 
-        # логика скрапера, надо винести в фоновою задачу
-        await join_channel(link)
+        join = await join_channel(link)
+        if join:
+            # Перевіряємо чи можливо зайти на канал
+            await message.answer("Такий username ніхто не використовує!")
+            await state.clear()
+            return
+
         title = await get_channel_title(link)
         channel_tg_id = await get_channel_id(link)
         user_id = message.from_user.id
 
+        # Додаємо в таблицю каналів цей канал
         await channel_repo.create_or_update_channel(channel_id=channel_tg_id, tg_link=link, title=title)
 
+        # Перевіряємо чи підписан юзер на цей канал
         user_subscribed = await subscription_repo.user_subscribed_to_channel(user_id=user_id, channel_id=channel_tg_id)
 
         if not user_subscribed:
