8059871188c31ffb02e0a24880792a6c6f2d0737
Бєлоглазов Олександр <beloalek@gmail.com>
2026-04-03 14:58:07 +0300

    Added logic for turn buttons and minor changes

--- Handlers/history.py
+++ Handlers/history.py
@@ -2,7 +2,7 @@ from aiogram import F, Router, types
 from aiogram.types import InlineKeyboardMarkup
 
 from Keyboard.history_list import generate_history_list_kb, ChannelPulseHistoryCBData, \
-    generate_channel_pulse_history_kb, PulseInfoCBData
+    generate_ch_pulse_history_kb, PulseInfoCBData, TurnPageCBData
 from Repositories.subscription import UserSubscriptionRepository
 from Repositories.summary import SummaryRepository
 
@@ -22,35 +22,42 @@ async def history(update: types.Message | types.CallbackQuery, subscription_repo
                 reply_markup=generate_history_list_kb(channels)
             )
         else:
-            await update.answer("Каналів не знайдено! Спочатку додайте канал.")
+            await update.answer("Список каналів порожній! Спочатку додайте канал.")
     # или юзер нажал кнопку "Back"
     else:
         await update.message.edit_text(
             "Це архів знань. Тут зібрані всі дайджести, які я готував для тебе раніше. Що саме хочеш переглянути?",
             reply_markup=generate_history_list_kb(channels)
         )
+        await update.answer()
 
+@router.callback_query(TurnPageCBData.filter()) # когда юзер нажал кнопку Next/Prev
 @router.callback_query(ChannelPulseHistoryCBData.filter()) # когда юзер нажал на канал из списка
-async def channel_pulse_history_list(callback_query: types.CallbackQuery, callback_data: ChannelPulseHistoryCBData, summary_repo: SummaryRepository):
+async def channel_pulse_history_list(callback_query: types.CallbackQuery, callback_data: ChannelPulseHistoryCBData | TurnPageCBData, summary_repo: SummaryRepository):
     summaries = await summary_repo.get_summaries_by_channel_id(callback_data.channel_id)
+    current_page = 0
+
+    if isinstance(callback_data, TurnPageCBData):
+        current_page = callback_data.current_page
 
     if summaries:
         await callback_query.message.edit_text(
-            "Pulse History:",
-            reply_markup=generate_channel_pulse_history_kb(summaries),
+            "Всі дайджести відсортованні по даті створення. Яку дату хочеш переглянути?",
+            reply_markup=generate_ch_pulse_history_kb(summaries=summaries, channel_id=callback_data.channel_id, curr_page=current_page)
         )
+        await callback_query.answer()
     else:
-        await callback_query.message.edit_text("Pulse history is empty! Get Pulse first!")
-
+        await callback_query.message.edit_text("Дайджестів поки немає! Створіть нові дайджести")
+        await callback_query.answer()
 
 @router.callback_query(PulseInfoCBData.filter()) # когда юзер нажал на дату дайджеста из списка дайджестов
 async def pulse_info(callback_query: types.CallbackQuery, callback_data: PulseInfoCBData, summary_repo: SummaryRepository):
     summary = await summary_repo.get_summary_by_post_id(callback_data.post_id)
 
     await callback_query.message.edit_text(
-        f"*Тема: «{summary.topic}»*\n"
+        f"📌 <b>«{summary.topic}»</b>\n"
         f"{summary.content}\n"
-        f"_Дата дайджесту: {summary.summary_date.strftime('%m-%d-%Y')}_",
+        f"📅 <i>Дата дайджесту: {summary.summary_date.strftime('%m-%d-%Y')}</i>",
         reply_markup=InlineKeyboardMarkup(
             inline_keyboard=[
                 [
@@ -61,6 +68,6 @@ async def pulse_info(callback_query: types.CallbackQuery, callback_data: PulseIn
                 ]
             ]
         ),
-        parse_mode="Markdown"
+        parse_mode="HTML"
     )
-
+    await callback_query.answer()
