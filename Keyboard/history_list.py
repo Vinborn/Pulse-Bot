37c07c2a7a3fa40dffe0699624e9e309e0d980ac
Бєлоглазов Олександр <beloalek@gmail.com>
2026-04-03 14:55:49 +0300

    Added turn buttons and set the keyboard layout

--- Keyboard/history_list.py
+++ Keyboard/history_list.py
@@ -1,9 +1,7 @@
 from aiogram.filters.callback_data import CallbackData
 from aiogram.types import InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-
-from Database import Summary
-
+from aiogram.types import InlineKeyboardMarkup
 
 class ChannelPulseHistoryCBData(CallbackData, prefix="ch_pulse_history"):
     channel_id: int
@@ -11,6 +9,10 @@ class ChannelPulseHistoryCBData(CallbackData, prefix="ch_pulse_history"):
 class PulseInfoCBData(CallbackData, prefix="pulse_info"):
     channel_id: int
     post_id: int
+
+class TurnPageCBData(CallbackData, prefix="turn_page"):
+    current_page: int
+    channel_id: int
 
 def generate_history_list_kb(channels):
     keyboard = InlineKeyboardBuilder()
@@ -24,16 +26,51 @@ def generate_history_list_kb(channels):
 
     return keyboard.as_markup()
 
-def generate_channel_pulse_history_kb(summaries: list[Summary]):
+def generate_ch_pulse_history_kb(summaries, channel_id:int, curr_page:int) -> InlineKeyboardMarkup:
+    all_buttons = [
+        InlineKeyboardButton(
+            text=summary.summary_date.strftime("%m/%d/%Y"),
+            callback_data=PulseInfoCBData(channel_id=summary.channel_id,
+                                          post_id=summary.last_included_post_id).pack()
+        )
+        for summary in summaries
+    ]
     keyboard = InlineKeyboardBuilder()
 
-    for summary in summaries:
-        keyboard.row(
+    # створюється grid 2х3 (2 колонки по 3 ряди)
+    buttons_for_grid = 6
+    start_index = curr_page * buttons_for_grid
+    end_index = (curr_page+1) * buttons_for_grid
+
+    keyboard.add(*all_buttons[start_index:end_index])
+    keyboard.adjust(2)
+
+    # Визначаємо максимальну кількість сторінок
+    sum_number = len(summaries)
+    max_pages = sum_number // buttons_for_grid
+
+    # Ряд кнопок для перегортання сторінок списку
+    turn_row = turn_buttons(channel_id=channel_id, current_page=curr_page, max_pages=max_pages)
+    keyboard.row(*turn_row)
+
+    keyboard.row(InlineKeyboardButton(text="Back to Channels", callback_data="history_list"))
+    return keyboard.as_markup()
+
+def turn_buttons(channel_id: int, current_page: int, max_pages: int) -> list[InlineKeyboardButton]:
+    buttons = []
+    if current_page > 0:
+        buttons.append(
             InlineKeyboardButton(
-                text=summary.summary_date.strftime("%m/%d/%Y"),
-                callback_data=PulseInfoCBData(channel_id=summary.channel_id, post_id=summary.last_included_post_id).pack()
+                text="← Prev", callback_data=TurnPageCBData(channel_id=channel_id, current_page=current_page - 1).pack()
             )
         )
 
-    keyboard.row(InlineKeyboardButton(text="Back", callback_data="history_list"))
-    return keyboard.as_markup()
+    if current_page < max_pages:
+        buttons.append(
+            InlineKeyboardButton(
+                text="Next →",
+                callback_data=TurnPageCBData(channel_id=channel_id, current_page=current_page + 1).pack()
+            )
+        )
+
+    return buttons
