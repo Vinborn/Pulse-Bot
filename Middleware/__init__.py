951b5fbe5ac09d100b388499ae3050363b54ba5e
Бєлоглазов Олександр <beloalek@gmail.com>
2026-04-25 15:17:29 +0300

    Register translation middleware

--- Middleware/__init__.py
+++ Middleware/__init__.py
@@ -1,6 +1,8 @@
 from aiogram import Dispatcher
 
 from Middleware.session import DatabaseSessionMiddleware
+from Middleware.translate import TranslationMiddleware
 
 def register_middleware(dp: Dispatcher, session_maker):
     dp.update.middleware(DatabaseSessionMiddleware(session_maker))
+    dp.update.middleware(TranslationMiddleware())
