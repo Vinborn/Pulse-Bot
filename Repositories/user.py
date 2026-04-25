2a040b0d949f395ed41671b183d9201e1073cc31
Бєлоглазов Олександр <beloalek@gmail.com>
2026-04-25 15:19:18 +0300

    Added get language function and minor changes

--- Repositories/user.py
+++ Repositories/user.py
@@ -9,32 +9,37 @@ class UserRepository:
     def __init__(self, session: AsyncSession):
         self.__session = session
 
-    async def get_user_by_tg_id(self, tg_id: int):
+    async def get_user_by_tg_id(self, tg_id: int) -> User:
         statement = select(User).where(User.tg_id == tg_id)
         return await self.__session.scalar(statement)
 
     # создаем юзера или обновляем данние
-    async def create_or_update_user(self, tg_id: int, username: str, language: str):
+    async def create_or_update_user(self, tg_id: int, username: str, tg_lang: str, ui_lang: str):
         # получаем из бази тг айди изера
         user = await self.get_user_by_tg_id(tg_id)
 
         # если юзера нету в базе, создаем его
         if not user:
-            await self.create_user(tg_id, username, language)
+            await self.create_user(tg_id, username, tg_lang, ui_lang)
         else:
             user.username = username
-            user.language = language
+            user.tg_language = tg_lang
 
         await self.__session.commit()
 
     # создаем юзера используя класс User и добавляем его в базу
-    async def create_user(self, tg_id: int, username: str, language: str) -> Any:
-        user = User(tg_id=tg_id, username=username, language=language)
+    async def create_user(self, tg_id: int, username: str, tg_lang: str, ui_lang: str):
+        user = User(tg_id=tg_id, username=username, tg_language=tg_lang, ui_language=ui_lang)
         self.__session.add(user)
 
-    async def change_language(self, tg_id: int, new_language: str):
+    async def set_language(self, tg_id: int, new_language: str):
         user = await self.get_user_by_tg_id(tg_id)
 
-        user.language = new_language
+        user.ui_language = new_language
 
         await self.__session.commit()
+
+    async def get_language(self, tg_id: int) -> str:
+        user = await self.get_user_by_tg_id(tg_id)
+
+        return user.ui_language
